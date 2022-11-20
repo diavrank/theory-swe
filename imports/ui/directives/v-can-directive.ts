7afebfa9f86173dfadc33f0cdfbb01fce2b8163f
diavrank <hec_ivan_150@hotmail.com>
2022-11-20 00:57:06 -0600

    Feat: Fix VCan directive

--- imports/ui/directives/v-can-directive.ts
+++ imports/ui/directives/v-can-directive.ts
@@ -1,46 +1,18 @@
-import {DirectiveBinding, VNode} from 'vue';
-import {Meteor} from 'meteor/meteor';
+import { DirectiveBinding, VNode } from 'vue';
+import { Meteor } from 'meteor/meteor';
 
-
-/**
- * Create comment node
- * @param el
- * @param vNode
- */
-function commentNode(el: HTMLElement, vNode: VNode) {
-    const comment = document.createComment(' ')
-
-    Object.defineProperty(comment, 'setAttribute', {
-        value: () => undefined
-    })
-    vNode.el = comment
-    vNode.appContext = null
-    vNode.scopeId = null
-    if (vNode.dirs) {
-        vNode.dirs = null
-    }
-
-    if (vNode.component) {
-        // @ts-ignore
-        vNode.component.el = comment
-    }
-
-    if (el.parentNode) {
-        el.parentNode.replaceChild(comment, el)
-    }
-}
-
-export const VCan = function (el: HTMLElement, binding: DirectiveBinding, vNode: VNode) {
-    const behaviour = binding.modifiers.disable ? 'disable' : 'hide';
-    // @ts-ignore
-    const hasPermission = Roles.userIsInRole(Meteor.userId(), `${binding.value}-${binding.arg}`,
-        Meteor.user()?.profile.profile);
-    if (!hasPermission) {
-        if (behaviour === 'hide') {
-            commentNode(el, vNode)
-        } else if (behaviour === 'disable') {
-            // @ts-ignore
-            el.disabled = true
-        }
-    }
-}
+export const VCan = function(el: HTMLElement, binding: DirectiveBinding, vNode: VNode) {
+	const behaviour = binding.modifiers.disable ? 'disable' : 'hide';
+	// @ts-ignore
+	const hasPermission = Roles.userIsInRole(Meteor.userId(), `${ binding.value }-${ binding.arg }`,
+		Meteor.user()?.profile.profile);
+	if (!hasPermission) {
+		if (behaviour === 'hide') {
+			// @ts-ignore
+			vNode.el.hidden = true;
+		} else if (behaviour === 'disable') {
+			// @ts-ignore
+			el.disabled = true;
+		}
+	}
+};
