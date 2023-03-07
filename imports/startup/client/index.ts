409a0fecaab68993e241e9dd68a8923646db2e3b
Erik Cabrera <erik_osva_150@hotmail.com>
2023-03-07 17:50:30 -0600

    Refactor: Removes vuex modules and configuration

--- imports/startup/client/index.ts
+++ imports/startup/client/index.ts
@@ -5,13 +5,10 @@ import { createApp } from 'vue';
 // Main app
 import App from '../../ui/App.vue';
 import router from '../../ui/router';
-import store from '../../ui/store';
 import { VueMeteor } from 'vue-meteor-tracker';
 import { VCan } from '/imports/ui/directives/v-can-directive';
 import AlertMessage from '/imports/ui/components/Utilities/Alerts/AlertMessage.vue';
 import Loader from '/imports/ui/components/Utilities/Loaders/Loader.vue';
-import { Store } from 'vuex';
-import { AuthState, NavigationState, TemporalState } from '/imports/ui/typings/vuex-store';
 import mitt, { Emitter, EventType } from 'mitt';
 import vuetify from '../../ui/plugins/vuetify';
 import { loadFonts } from '/imports/ui/plugins/webfontloader';
@@ -28,26 +25,13 @@ declare module 'vue' {
 	}
 }
 
-declare module '@vue/runtime-core' {
-	// Declare your own store states.
-	interface State {
-		auth: AuthState,
-		temporal: TemporalState,
-		navigation: NavigationState,
-	}
-
-	interface ComponentCustomProperties {
-		$store: Store<State>;
-	}
-}
-
 Meteor.startup(() => {
 	loadFonts();
 	const emitter = mitt();
 	const pinia = createPinia();
+	// @ts-ignore
 	const app = createApp(App);
 	app.use(pinia);
-	app.use(store);
 	app.use(router);
 	app.use(VueMeteor);
 	app.use(vuetify);
