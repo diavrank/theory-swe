5f10cc86880bc5cc45fc82c1f84f1918bba1b292
Erik Cabrera <erik_osva_150@hotmail.com>
2023-03-08 23:11:40 -0600

    Refactor: Updates ResetPassword.vue component with the composition API.

--- imports/ui/views/Auth/ResetPassword.vue
+++ imports/ui/views/Auth/ResetPassword.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <div class="title">Reset password</div>
-      <Form as="v-form" @submit.prevent="resetPassword" ref="setPasswordFormObserver">
-        <Field v-slot="{errors}" name="new password" vid="confirmation"
-                            rules="strength_password|required">
-          <v-text-field v-model="user.password" id="inputNewPassword"
+    <Form as="v-form" v-slot="{handleSubmit}" ref="setPasswordFormObserver">
+      <v-form @submit="handleSubmit($event, resetPassword)" autocomplete="off">
+        <Field v-slot="{field, errors}" name="password" rules="strength_password|required">
+          <v-text-field v-bind="field"
+                        v-model="user.password" id="inputNewPassword"
                         :type="showPass.new ? 'text' : 'password'"
                         name="password"
                         :error-messages="errors"
@@ -12,24 +13,25 @@
                         autocomplete="new-password"
                         data-test-id="password-input"
                         required>
-            <template v-slot:append>
-              <v-btn icon @click="showPass.new = !showPass.new" tabindex="-1">
+            <template #append-inner>
+              <v-btn icon variant="text" @click="showPass.new = !showPass.new" tabindex="-1">
                 <v-icon v-if="!showPass.new">mdi:mdi-eye</v-icon>
                 <v-icon v-else>mdi:mdi-eye-off</v-icon>
               </v-btn>
             </template>
           </v-text-field>
         </Field>
-        <Field v-slot="{errors}" name="confirm password" rules="required|confirmed:confirmation">
+        <Field v-slot="{field, errors}" name="confirm password" rules="required|confirmed:@password">
           <v-text-field v-model="user.confirmPassword" id="inputConfirmPassword"
+                        v-bind="field"
                         :type="showPass.confirm ? 'text' : 'password'"
                         :error-messages="errors"
                         name="password_confirmation"
                         label="Confirm password"
                         data-test-id="input-password-confirmation"
                         required>
-            <template v-slot:append>
-              <v-btn icon @click="showPass.confirm = !showPass.confirm" tabindex="-1">
+            <template #append-inner>
+              <v-btn icon variant="text" @click="showPass.confirm = !showPass.confirm" tabindex="-1">
                 <v-icon v-if="!showPass.confirm">mdi:mdi-eye</v-icon>
                 <v-icon v-else>mdi:mdi-eye-off</v-icon>
               </v-btn>
@@ -39,16 +41,19 @@
         <div class="d-flex start mt-2">
           <v-btn type="submit" color="primary" rounded data-test-id="change-button">Change</v-btn>
         </div>
-      </Form>
+      </v-form>
+    </Form>
 
   </div>
 </template>
 
 <script lang="ts">
-import {Form, Field, FormContext} from 'vee-validate';
-import  {defineComponent} from 'vue';
-import validateForm from '@mixins/validateForm';
-import { Meteor } from 'meteor/meteor';
+import { Form, Field } from 'vee-validate';
+import { defineComponent, inject, reactive, ref } from 'vue';
+import { useFormValidation } from '/imports/ui/composables/forms';
+import { Injections } from '@typings/utilities';
+import { AlertMessageType } from '@components/Utilities/Alerts/AlertMessage.vue';
+import { useRoute, useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'ResetPassword',
@@ -56,38 +61,36 @@ export default defineComponent({
     Form,
     Field
   },
-  mixins: [validateForm],
-  data() {
-    return {
-      user: {
-        password: null,
-        confirmPassword: null
-      },
-      showPass: {
-        new: false,
-        confirm: false
+  setup() {
+    const setPasswordFormObserver = ref(null);
+    const route = useRoute();
+    const router = useRouter();
+    const alert = inject<AlertMessageType>(Injections.AlertMessage);
+    const user = reactive({
+      password: null,
+      confirmPassword: null
+    });
+    const showPass = reactive({
+      new: false,
+      confirm: false
+    });
+    const resetPassword = async () => {
+      if (await useFormValidation(setPasswordFormObserver.value, alert)) {
+        const token = route.params.token;
+        Accounts.resetPassword(token, user.password || '', (error: MediaError) => {
+          if (error) {
+            console.error('An error occurred while resetting the password', error);
+            alert?.showAlertSimple('error', 'An error occurred while resetting the password');
+          } else {
+            alert?.showAlertSimple('success', 'Password reset successfully');
+            router.push({name: 'login'});
+          }
+        })
       }
     };
-  },
-  methods: {
-    async resetPassword() {
-      if (await this.isFormValid(this.$refs.setPasswordFormObserver as FormContext)) {
-        const token = this.$route.params.token as string;
-        Accounts.resetPassword(token, this.user.password || '', (err: Error | Meteor.Error | Meteor.TypedError | undefined) => {
-          if (err) {
-            console.error('An error occurred while resetting the password', err);
-            this.$alert.showAlertSimple('error', 'An error occurred while resetting the password');
-          } else {
-            this.$alert.showAlertSimple('success', 'Password reset successfully!');
-            this.$router.push({name: 'login'});
-          }
-        });
-      }
-    }
+
+    return {setPasswordFormObserver, user, showPass, resetPassword};
   }
 })
 </script>
 
-<style scoped>
-
-</style>
