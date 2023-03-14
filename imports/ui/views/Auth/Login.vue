9af991a3a6ef8d798452284cbae1b12b68f9a0a3
Erik Cabrera <erik_osva_150@hotmail.com>
2023-03-13 20:25:06 -0600

    Fix: Unwraps v-form from Vee Validate Form observer, which forces page reload

--- imports/ui/views/Auth/Login.vue
+++ imports/ui/views/Auth/Login.vue
@@ -2,17 +2,19 @@
   <div class="login-wrapper">
     <div class="title secondary--text">Welcome!</div>
     <div class="display-1 mb-0 secondary--text">Login</div>
-    <Form as="div" :initial-values="initialValues" ref="loginObserver">
-      <v-form @submit.prevent="login" autocomplete="off">
+    <v-form @submit.prevent="login" autocomplete="off">
+      <Form as="div" :initial-values="initialValues" ref="loginObserver">
         <Field v-slot="{ field, errors }" name="username" rules="required">
-          <v-text-field v-bind="field" name="username" v-model="user.userOrEmail" autocomplete="off" required color="primary"
+          <v-text-field v-bind="field" name="username" v-model="user.userOrEmail" autocomplete="off" required
+                        color="primary"
                         type="text" :error-messages="errors" label="Username"
                         data-test-id="input-user"
                         prepend-icon="person">
           </v-text-field>
         </Field>
         <Field v-slot="{ field, errors }" name="password" rules="required">
-          <v-text-field v-bind="field" name="password" label="Password" type="password" prepend-icon="lock" autocomplete="current-password"
+          <v-text-field v-bind="field" name="password" label="Password" type="password" prepend-icon="lock"
+                        autocomplete="current-password"
                         :error-messages="errors" v-model="user.password" required data-test-id="input-password">
           </v-text-field>
         </Field>
@@ -24,8 +26,8 @@
         <div class="d-flex justify-start">
           <v-btn type="submit" rounded color="primary" transition="fade" data-test-id="login-button">Enter</v-btn>
         </div>
-      </v-form>
-    </Form>
+      </Form>
+    </v-form>
   </div>
 </template>
 
@@ -64,19 +66,19 @@ export default defineComponent({
     })
 
     const successLogin = () => {
-      Meteor.logoutOtherClients((errorResponse: MeteorError ) => {
+      Meteor.logoutOtherClients((errorResponse: MeteorError) => {
         if (errorResponse) {
           console.error('Error to logout other clients: ', errorResponse);
         }
       });
       alert?.closeAlert();
       authStore.setUser(Meteor.user() as User);
-      router.push({ name: 'home' });
+      router.push({name: 'home'});
     }
 
     const login = async () => {
       const observer = loginObserver.value as FormContext | null;
-      if (observer !== null && alert && await useFormValidation(observer, alert)){
+      if (observer !== null && alert && await useFormValidation(observer, alert)) {
         loader?.activate('Logging in . . .');
         Meteor.loginWithPassword(user.userOrEmail, user.password, (errorResponse: MeteorError) => {
           loader?.deactivate();
@@ -100,7 +102,7 @@ export default defineComponent({
       loginObserver.value?.resetForm();
     });
 
-    return { authStore, loginObserver, user, initialValues, login };
+    return {authStore, loginObserver, user, initialValues, login};
   }
 });
 </script>
