58437680065abbbd997e33fd319cb47db13f534c
Iván Cabrera <diavrank@Ivans-MacBook-Pro.local>
2023-12-09 04:25:10 -0600

    docs: add instructions to generate the keys

--- imports/ui/views/DigitalSignature/DigitalSignature.vue
+++ imports/ui/views/DigitalSignature/DigitalSignature.vue
@@ -7,6 +7,29 @@
         </v-row>
 
         <v-row>
+            <v-col cols="12" md="12">
+                <v-card>
+                    <v-card-title>
+                        Instructions to generate the keys
+                    </v-card-title>
+                    <v-card-text>
+                        Create your own private key with following command:
+                        <v-code class="ma-4">
+                            openssl genpkey -algorithm RSA -pkeyopt
+                            rsa_keygen_bits:4096 -pkeyopt rsa_keygen_pubexp:3
+                            -out privateKey.pem
+                        </v-code>
+                        Create your public key for above private key:
+                        <v-code class="ma-4">
+                            openssl pkey -in privateKey.pem -out publicKey.pem
+                            -pubout
+                        </v-code>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row>
             <v-col cols="12" xs="12" sm="6" md="6">
                 <SignFile />
             </v-col>
