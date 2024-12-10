3daa02367330f835bcf812f1fa733b5948f45157
Iván Cabrera <diavrank@Ivans-MacBook-Pro.local>
2024-12-10 17:39:27 -0600

    fix: update roles package

--- imports/ui/views/Auth/ResetPassword.vue
+++ imports/ui/views/Auth/ResetPassword.vue
@@ -1,44 +1,46 @@
 <template>
   <div>
     <div class="title">Reset password</div>
-      <Form as="v-form" @submit.prevent="resetPassword" ref="setPasswordFormObserver">
-        <Field v-slot="{errors}" name="new password" vid="confirmation"
-                            rules="strength_password|required">
-          <v-text-field v-model="user.password" id="inputNewPassword"
-                        :type="showPass.new ? 'text' : 'password'"
-                        name="password"
-                        :error-messages="errors"
-                        label="New password"
-                        autocomplete="new-password"
-                        data-test-id="password-input"
-                        required>
-            <template v-slot:append>
-              <v-btn icon @click="showPass.new = !showPass.new" tabindex="-1">
-                <v-icon v-if="!showPass.new">mdi:mdi-eye</v-icon>
-                <v-icon v-else>mdi:mdi-eye-off</v-icon>
-              </v-btn>
-            </template>
-          </v-text-field>
-        </Field>
-        <Field v-slot="{errors}" name="confirm password" rules="required|confirmed:confirmation">
-          <v-text-field v-model="user.confirmPassword" id="inputConfirmPassword"
-                        :type="showPass.confirm ? 'text' : 'password'"
-                        :error-messages="errors"
-                        name="password_confirmation"
-                        label="Confirm password"
-                        data-test-id="input-password-confirmation"
-                        required>
-            <template v-slot:append>
-              <v-btn icon @click="showPass.confirm = !showPass.confirm" tabindex="-1">
-                <v-icon v-if="!showPass.confirm">mdi:mdi-eye</v-icon>
-                <v-icon v-else>mdi:mdi-eye-off</v-icon>
-              </v-btn>
-            </template>
-          </v-text-field>
-        </Field>
-        <div class="d-flex start mt-2">
-          <v-btn type="submit" color="primary" rounded data-test-id="change-button">Change</v-btn>
-        </div>
+      <Form as="div" v-slot="{handleSubmit}" ref="setPasswordFormObserver">
+        <v-form @submit="handleSubmit($event,resetPassword)" autocomplete="off">
+          <Field v-slot="{ field, errors}" name="new password" ref="confirmation"
+                              rules="strength_password|required">
+            <v-text-field v-bind="field" v-model="user.password" id="inputNewPassword"
+                          :type="showPass.new ? 'text' : 'password'"
+                          name="password"
+                          :error-messages="errors"
+                          label="New password"
+                          autocomplete="new-password"
+                          data-test-id="password-input"
+                          required>
+              <template v-slot:append>
+                <v-btn icon @click="showPass.new = !showPass.new" tabindex="-1">
+                  <v-icon v-if="!showPass.new">mdi:mdi-eye</v-icon>
+                  <v-icon v-else>mdi:mdi-eye-off</v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
+          </Field>
+          <Field v-slot="{ field, errors}" name="confirm password" rules="required|confirmed:confirmation">
+            <v-text-field v-bind="field" v-model="user.confirmPassword" id="inputConfirmPassword"
+                          :type="showPass.confirm ? 'text' : 'password'"
+                          :error-messages="errors"
+                          name="password_confirmation"
+                          label="Confirm password"
+                          data-test-id="input-password-confirmation"
+                          required>
+              <template v-slot:append>
+                <v-btn icon @click="showPass.confirm = !showPass.confirm" tabindex="-1">
+                  <v-icon v-if="!showPass.confirm">mdi:mdi-eye</v-icon>
+                  <v-icon v-else>mdi:mdi-eye-off</v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
+          </Field>
+          <div class="d-flex start mt-2">
+            <v-btn type="submit" color="primary" rounded data-test-id="change-button">Change</v-btn>
+          </div>
+        </v-form>
       </Form>
 
   </div>
